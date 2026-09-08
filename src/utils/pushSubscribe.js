b5b3b5f49bb1a054826bcfb6715ad88b54cc0749
Asser-wael <aserweal@gmail.com>
2026-09-08 13:38:32 +0300

    update client

--- src/utils/pushSubscribe.js
+++ src/utils/pushSubscribe.js
@@ -1,49 +1,46 @@
 import axiosInstance from "../api/axiosInstance";
+import { store } from "../app/store";
+import { showToast } from "./showToast";
 
 export async function subscribeToPush() {
   try {
-    const registration =
-      await navigator.serviceWorker.register("/sw.js");
+    const register = await navigator.serviceWorker.register("/sw.js");
 
-    await navigator.serviceWorker.ready;
-
-    const permission =
-      await Notification.requestPermission();
+    const permission = await Notification.requestPermission();
 
     if (permission !== "granted") return;
 
-    let subscription =
-      await registration.pushManager.getSubscription();
+
+    let subscription = await register.pushManager.getSubscription();
 
     if (!subscription) {
-      subscription =
-        await registration.pushManager.subscribe({
-          userVisibleOnly: true,
-          applicationServerKey:
-            urlBase64ToUint8Array(
-              import.meta.env.VITE_VAPID_PUBLIC_KEY
-            ),
-        });
+      subscription = await register.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(
+          import.meta.env.VITE_VAPID_PUBLIC_KEY
+        ),
+      });
     }
 
-    console.log("Subscription:", subscription);
 
-    await axiosInstance.post(
-      "/notifications/subscribe",
-      { subscription }
-    );
+    const res = await axiosInstance.post("/notifications/subscribe", {
+      subscription,
+    });
 
-    console.log("Push subscribed!");
-  } catch (error) {
-    console.error("Push error:", error);
+
+
+  } catch (err) {
+    console.log(err);
+    console.log(err.response);
+    console.log(err.response?.data);
   }
 }
 
+
 function urlBase64ToUint8Array(base64String) {
-  const padding =
-    "=".repeat(
-      (4 - (base64String.length % 4)) % 4
-    );
+  const padding = "=".repeat(
+    (4 - (base64String.length % 4)) % 4
+  );
 
   const base64 = (base64String + padding)
     .replace(/-/g, "+")
@@ -52,6 +49,6 @@ function urlBase64ToUint8Array(base64String) {
   const raw = atob(base64);
 
   return Uint8Array.from(
-    [...raw].map((char) => char.charCodeAt(0))
+    [...raw].map((c) => c.charCodeAt(0))
   );
 }
