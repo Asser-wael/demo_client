2d5a060371c7d05cc853cb272955486e79ff9a45
Asser-wael <aserweal@gmail.com>
2026-09-09 18:10:49 +0300

    update client

--- src/utils/pushSubscribe.js
+++ src/utils/pushSubscribe.js
@@ -5,26 +5,33 @@ import { showToast } from "./showToast";
 export async function subscribeToPush() {
   try {
     const register = await navigator.serviceWorker.register("/sw.js");
-    console.log(1);
 
     const permission = await Notification.requestPermission();
-    console.log(2);
 
     if (permission !== "granted") return;
-    console.log(3);
 
 
     let subscription = await register.pushManager.getSubscription();
-    console.log(4);
 
     if (!subscription) {
-      console.log(5);
+      console.log("1 - subscription:", subscription);
+
+      console.log("2 - VAPID:", import.meta.env.VITE_VAPID_PUBLIC_KEY);
+
+      console.log("3 - register:", register);
+
+      const key = urlBase64ToUint8Array(
+        import.meta.env.VITE_VAPID_PUBLIC_KEY
+      );
+
+      console.log("4 - converted key:", key);
+
       subscription = await register.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(
-          import.meta.env.VITE_VAPID_PUBLIC_KEY
-        ),
+        applicationServerKey: key,
       });
+
+      console.log("5 - SUCCESS:", subscription);
     }
     console.log(6);
 
