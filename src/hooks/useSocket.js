8021da850a4a83bddf597e30103da90587f80c8d
Asser-wael <aserweal@gmail.com>
2026-09-09 19:15:58 +0300

    update client

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -1,12 +1,17 @@
+// hooks/useSocket.js
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const useSocket = () => {
+const useSocket = (token) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
     const socketInstance = io(import.meta.env.VITE_SOCKET_URL, {
       withCredentials: true,
+      auth: {
+        token: token, 
+      },
+      transports: ["websocket", "polling"],
     });
 
     setSocket(socketInstance);
@@ -14,7 +19,7 @@ const useSocket = () => {
     return () => {
       socketInstance.disconnect();
     };
-  }, []);
+  }, [token]);
 
   return socket;
 };
