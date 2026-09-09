8e1b0954565a4bcf95c767818b041bb3950d4e21
Asser-wael <aserweal@gmail.com>
2026-09-09 18:02:03 +0300

    update client

--- src/App.jsx
+++ src/App.jsx
@@ -225,6 +225,7 @@ function App() {
   ========================================================= */
 
   useEffect(() => {
+    if (!user) return;
     dispatch(getCart());
   }, [dispatch]);
 
--- src/utils/pushSubscribe.js
+++ src/utils/pushSubscribe.js
@@ -5,34 +5,34 @@ import { showToast } from "./showToast";
 export async function subscribeToPush() {
   try {
     const register = await navigator.serviceWorker.register("/sw.js");
-console.log(1);
+    console.log(1);
 
-const permission = await Notification.requestPermission();
-console.log(2);
+    const permission = await Notification.requestPermission();
+    console.log(2);
 
-if (permission !== "granted") return;
-console.log(3);
-
-
-let subscription = await register.pushManager.getSubscription();
-console.log(4);
-
-if (!subscription) {
-  console.log(5);
-  subscription = await register.pushManager.subscribe({
-    userVisibleOnly: true,
-    applicationServerKey: urlBase64ToUint8Array(
-      import.meta.env.VITE_VAPID_PUBLIC_KEY
-    ),
-  });
-}
-console.log(6);
+    if (permission !== "granted") return;
+    console.log(3);
 
 
-const res = await axiosInstance.post("/notifications/subscribe", {
-  subscription,
-});
-console.log(7);
+    let subscription = await register.pushManager.getSubscription();
+    console.log(4);
+
+    if (!subscription) {
+      console.log(5);
+      subscription = await register.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(
+          import.meta.env.VITE_VAPID_PUBLIC_KEY
+        ),
+      });
+    }
+    console.log(6);
+
+
+    const res = await axiosInstance.post("/notifications/subscribe", {
+      subscription,
+    });
+    console.log(7);
 
 
 
