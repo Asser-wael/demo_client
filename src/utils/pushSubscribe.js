95c7bd0688512f9fcf5ec6543b7fa8fa1d109fa2
Asser-wael <aserweal@gmail.com>
2026-09-13 23:22:57 +0300

    update client

--- src/utils/pushSubscribe.js
+++ src/utils/pushSubscribe.js
@@ -5,18 +5,25 @@ import { showToast } from "./showToast";
 
 export async function subscribeToPush() {
   try {
+    console.log(1);
+    
     const register = await navigator.serviceWorker.register("/sw.js");
-
+    console.log(2);
+    
     const permission = await Notification.requestPermission();
-
+    console.log(3);
+    
     if (permission !== "granted") {
       console.log("Notification permission denied");
       return;
     }
-
+    console.log(4);
+    
     let subscription = await register.pushManager.getSubscription();
-
+    console.log(5);
+    
     if (!subscription) {
+      console.log(6);
       subscription = await register.pushManager.subscribe({
         userVisibleOnly: true,
         applicationServerKey: urlBase64ToUint8Array(
@@ -24,11 +31,13 @@ export async function subscribeToPush() {
         ),
       });
     }
-
-
+    
+    console.log(7);
+    
     const res = await axiosInstance.post("/notifications/subscribe", {
       subscription,
     });
+    console.log(8);
 
 
 
