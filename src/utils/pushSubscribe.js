a0491e2c3962a0d5fe93e11639a6f5decd1ea666
Asser-wael <aserweal@gmail.com>
2026-09-13 16:31:01 +0300

    update client

--- src/utils/pushSubscribe.js
+++ src/utils/pushSubscribe.js
@@ -29,11 +29,11 @@ export async function subscribeToPush() {
 
     if (!subscription) {
       console.log(import.meta.env.VITE_VAPID_PUBLIC_KEY);
-      
+
       const key = urlBase64ToUint8Array(
         import.meta.env.VITE_VAPID_PUBLIC_KEY
       );
-console.log(key);
+      console.log(key);
 
       subscription = await registration.pushManager.subscribe({
         userVisibleOnly: true,
@@ -49,4 +49,61 @@ console.log(key);
   } catch (error) {
     console.error("Push subscribe failed:", error);
   }
+}
+
+import axiosInstance from "../api/axiosInstance";
+import { store } from "../app/store";
+import { showToast } from "./showToast";
+
+export async function subscribeToPush() {
+  try {
+    const register = await navigator.serviceWorker.register("/sw.js");
+
+    const permission = await Notification.requestPermission();
+
+    if (permission !== "granted") {
+      console.log("Notification permission denied");
+      return;
+    }
+
+    let subscription = await register.pushManager.getSubscription();
+
+    if (!subscription) {
+      subscription = await register.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(
+          import.meta.env.VITE_VAPID_PUBLIC_KEY
+        ),
+      });
+    }
+
+
+    const res = await axiosInstance.post("/notifications/subscribe", {
+      subscription,
+    });
+
+
+
+  } catch (err) {
+    console.log(err);
+    console.log(err.response);
+    console.log(err.response?.data);
+  }
+}
+
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat(
+    (4 - (base64String.length % 4)) % 4
+  );
+
+  const base64 = (base64String + padding)
+    .replace(/-/g, "+")
+    .replace(/_/g, "/");
+
+  const raw = atob(base64);
+
+  return Uint8Array.from(
+    [...raw].map((c) => c.charCodeAt(0))
+  );
 }
