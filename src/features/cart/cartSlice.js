0272809fc8467d7f9aa2b6c5e2fb3511b9e405e0
Asser-wael <aserweal@gmail.com>
2026-09-13 17:13:19 +0300

    update client

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -12,6 +12,7 @@ export const getCart = createAsyncThunk(
   async (_, { rejectWithValue }) => {
     try {
       const { data } = await axiosInstance.get("/cart");
+      console.log(data.cart);
       
       return data.cart;
     } catch (error) {
