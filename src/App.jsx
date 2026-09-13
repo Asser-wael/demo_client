d4c2b154956c028ec5e4f40c12e1444617cae9ad
Asser-wael <aserweal@gmail.com>
2026-09-13 18:11:05 +0300

    update client

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import { getUser } from "./features/auth/authSlice.js";
 import { getCart } from "./features/cart/cartSlice.js";
 import { addOrder } from "./features/order/orderSlice.js";
 import useApplyTheme from "./hooks/useApplyTheme.js";
+import { fetchSettings } from "./features/settings/settingsSlice.js";
 
 function App() {
   useApplyTheme();
