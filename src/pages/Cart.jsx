a54476610b1ea0b5b3a6fd7f2c39bf50e67a3e82
Asser-wael <aserweal@gmail.com>
2026-09-14 01:31:10 +0300

    update client

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -9,7 +9,6 @@ import {
 } from "../features/cart/cartSlice";
 import { useNavigate } from "react-router-dom";
 import Loading from "../components/common/Loading";
-import Currency from "../components/company/Currency";
 
 
 const containerVariants = {
